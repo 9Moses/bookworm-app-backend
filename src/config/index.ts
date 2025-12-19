68ae1756e3e9a0f885ce855258387bb96da6626c
9Moses <esselmoses12@gmail.com>
2025-12-19 15:36:04 +0000

    updates db

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const config = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
-    WHITELISTED_ORIGINS: [process.env.WHITELISTED_ORIGINS],
+    WHITELISTED_ORIGINS: [process.env.WHITELISTED_ORIGINS || "http://localhost:8081"],
     MONGO_URL: process.env.MONGO_URL,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
